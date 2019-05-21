d2f2f2c57b19a5957183522ae29b2cc52827d9cb
rbhanot4739 <rbhanot@tower-research.com>
2019-05-21 19:16:13 +0530

    JSOnweb

--- dummy.go
+++ dummy.go
@@ -1,33 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	valid := []interface{}{"a", "b", "c"}
-	invalid := []interface{}{}
-	hostgroups := []interface{}{"b", "d", "f"}
-	// var final []interface{}
-
-	for i, val := range valid {
-		if !SliceContains(hostgroups, val) {
-			invalid = append(invalid, val)
-			valid = append(valid[:i], valid[i+1:]...)
-		}
-	}
-	// valid = final
-	// final = nil
-	// delete final
-	fmt.Printf("%v -- %p\n", valid, &valid)
-
-	fmt.Println(invalid)
-	fmt.Println(hostgroups)
-}
-
-func SliceContains(a []interface{}, v interface{}) bool {
-	for _, val := range a {
-		if v == val {
-			return true
-		}
-	}
-	return false
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Println(rand.Intn(99), rand.Intn(99))
 }
